0b9640def0760017a324105303ca3f374582424e
irialvz <iriiachan11@gmail.com>
2025-03-24 22:05:06 +0100

    ejercicio clase

--- UD11/src/ejemploBD/dao/Main.java
+++
@@ -1,5 +0,0 @@
-package ejemploBD.dao;
-
-public class Main {
-
-}
--- UD11/src/ejemploBD/dao/Principal.java
+++ UD11/src/ejemploBD/dao/Principal.java
@@ -93,33 +93,80 @@ public class Principal {
 
 				case 4:
 					// "4) Modificar ubicacion de un departamento
+					String nuevaUbi = Teclado.leerCadena("Ubicacion nueva?");
+					int codDep = Teclado.leerEntero("Codigo del departamento?");
+					boolean modificado = AccesoDepartamento.modificarDepartamento(nuevaUbi, codDep);
+					if (!modificado) {
+						System.out.println("No se ha encontrado ningun departamento con ese codigo");
+					}else {
+						System.out.println("Se ha modificado correctamento");
+						Departamento dp = AccesoDepartamento.consultarDepartamento(codDep);
+						System.out.println(dp.toString());
+					}
 					break;
 				case 5:
 					// 5) Consultar departamento por codigo
+					int codDep5 = Teclado.leerEntero("Codigo del departamento");
+					Departamento dep5 = AccesoDepartamento.consultarDepartamento(codDep5);
+					if (dep5 != null) {
+						System.out.println(dep5.toString());
+					} else {
+						System.out.println("No existe ningun departamento con ese codigo");
+					}
 					break;
 				case 6:
 					// agregar departamento
-					
+					String nuevoNombre6 = Teclado.leerCadena("Nombre del nuevo departamento?");
+					String nuevaUbi6 = Teclado.leerCadena("Ubicacion del nuevo departamento?");
+					boolean agregado6 = AccesoDepartamento.agregarDepartamento(nuevoNombre6, nuevaUbi6);
+					if (agregado6) {
+						System.out.println("Se ha agregado correctamento");
+						Departamento ultDep = AccesoDepartamento.listarDepartamentos().getLast();
+						System.out.println(ultDep.toString());
+					} else {
+						System.out.println("El departamento no se ha podido agregar");
+					}
 					break;
 				case 7:
 					int codigoEmpleado = Teclado.leerEntero("Codigo del empleado? ");
-					int codigoNuevoD = Teclado.leerEntero("Codigo del departamento nuevo? ");
-
-					
-					/*if (AccesoDepartamento.consultarDepartamento(codigoEmpleado).getCodigo() != codigoEmpleado) {
-						System.out.println("No existe ese codigo de departamento");
-					}*/
-					
+					Empleado emp7 = AccesoEmpleado.consultarEmpleadoCodigo(codigoEmpleado);
+					while (emp7 == null) {
+						System.out.println("No existe un empleado con ese codigo");
+						codigoEmpleado = Teclado.leerEntero("Codigo del empleado?");
+						emp7 = AccesoEmpleado.consultarEmpleadoCodigo(codigoEmpleado);
+					}
+					System.out.println(emp7.toString());
+					System.out.println("\nLISTA DE DEPARTAMENTOS");
 					List<Departamento> listaDepartamentos = AccesoDepartamento.listarDepartamentos();
-					System.out.println(listaDepartamentos);
+					for (Departamento d : listaDepartamentos) {
+						System.out.println(d.toString());
+					}
+					System.out.println();
 					
-					boolean empleado;
-					empleado = AccesoEmpleado.cambiarDepartamentoEmpleado(codigoEmpleado, codigoNuevoD);
-
-					if (!empleado) {
-						System.out.println("No se ha encontrado ningún empleado con ese codigo en la base de datos.");
-					} else {
+					String nomDep7 = Teclado.leerCadena("Nombre del departamento? ");
+					boolean existe = AccesoDepartamento.existeDepartamentoNombre(nomDep7);
+					while (!existe) {
+						System.out.println("No existe un departamento con ese nombre");
+						boolean agregarDep = Teclado.leerBooleano("Agregar departamento con ese nombre? true/false");
+						if (agregarDep) {
+							String ubicacionNuevo7 = Teclado.leerCadena("Ubicacion del departamento nuevo?");
+							boolean agregado = AccesoDepartamento.agregarDepartamento(nomDep7, ubicacionNuevo7);
+							if (agregado) {
+								existe = true;
+							}
+						} else {
+							System.out.println("No se va a agregar un departamento");
+							return;
+						}
+					}
+					
+					boolean empleado = AccesoEmpleado.cambiarDepartamentoEmpleado(codigoEmpleado, nomDep7);
+					if (empleado) {
 						System.out.println("Se ha cambiado correctamento");
+						Empleado em = AccesoEmpleado.consultarEmpleadoCodigo(codigoEmpleado);
+						System.out.println(em.toString());
+					}else {
+						System.out.println("No se ha podido cambiar el departamento");
 					}
 					break;
 				case 8:
@@ -128,7 +175,9 @@ public class Principal {
 					if (empleados2.isEmpty()) {
 						System.out.println("No se ha encontrado ningun empleado");
 					} else {
-						System.out.println(empleados2);
+						for (Empleado e : empleados2) {
+							System.out.println(e.toString());
+						}
 					}
 					break;
 					
@@ -138,7 +187,9 @@ public class Principal {
 					if (departamentosAll.isEmpty()) {
 						System.out.println("La lista esta vacia, no hay departamentos");
 					} else {
-						System.out.println(departamentosAll);
+						for (Departamento e : departamentosAll) {
+							System.out.println(e.toString());
+						}
 					}
 				}
 				
