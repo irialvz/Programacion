17530d608e568cfc00823681b2dc8d61a7ed8229
daw_manana_207 <iriaalvarezsolis@gmail.com>
2025-04-25 13:06:02 +0200

    ejercicio json

--- EjercicioJson/src/com/ejemplo/json/JsonUtil.java
+++ EjercicioJson/src/com/ejemplo/json/JsonUtil.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Type;
 import java.util.*;
 import java.util.ArrayList;
 import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
 import java.io.Reader;
 
 public class JsonUtil {
@@ -15,5 +17,14 @@ public class JsonUtil {
 	}
 	public static Persona convetirPersonaJsonObj(String json) {
 		return GSON.fromJson(json, Persona.class);
+	}
+	public static ArrayList<Persona> convertirArchivoJsonALista(FileReader reader){
+		//CUANDO COGE LA LISTA DENTRO DEL ARCHIVO LA TRANSFORMA DIRECTAMENTE 
+		Type listaTipo = new TypeToken<ArrayList<Persona>>() {}.getType();
+		return GSON.fromJson(reader, listaTipo);
+	}
+	public static ArrayList<Persona> convertirJsonALista(String json){
+		Type listaTipo = new TypeToken<ArrayList<Persona>>() {}.getType();
+		return GSON.fromJson(json, listaTipo);
 	}
 }
--- EjercicioJson/src/com/ejemplo/json/Main.java
+++ EjercicioJson/src/com/ejemplo/json/Main.java
@@ -1,5 +1,8 @@
 package com.ejemplo.json;
-import 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 
@@ -15,9 +18,14 @@ public class Main {
 	
 	try {
 		System.out.println("\nArchivo lista JSON a ArrayList:");
-		
+		FileReader reader = new FileReader("personas.json");
+		ArrayList<Persona> listaDesdeArchivo = JsonUtil.convertirArchivoJsonALista(reader);
+		System.out.println("Contenido del archivo a json: \n");
+		for (Persona persona : listaDesdeArchivo) {
+			System.out.println(persona);
+		}
 	}catch (Exception ex) {
-		System.out.println();
+		System.err.println("Error al leer el archivo Json " + ex.getMessage());
 	}
 	
 	
