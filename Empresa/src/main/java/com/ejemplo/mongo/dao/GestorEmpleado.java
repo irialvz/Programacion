5f50803ecbb00e4785ece85bfb4a63698396f51c
daw_manana_207 <iriaalvarezsolis@gmail.com>
2025-04-25 13:32:04 +0200

    clases empresa y mongo

---
+++ Empresa/src/main/java/com/ejemplo/mongo/dao/GestorEmpleado.java
@@ -0,0 +1,50 @@
+package com.ejemplo.mongo.dao;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gte;
+
+import java.util.ArrayList;
+
+import org.bson.Document;
+
+import com.ejemplo.mongo.modelo.Empleado;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+
+public class GestorEmpleado {
+
+	public static String buscarEmpleadoNombreEdad(MongoCollection<Document> collection, String nombre, int edad) throws Exception {
+		// TODO Auto-generated method stub
+		String json = "";
+
+		// Filtro: nombre = "María" y edad >= 30
+		FindIterable<Document> resultados = collection.find(and(eq("nombre", nombre), gte("edad", edad)));
+		
+		for (Document doc : resultados) {
+			json += doc.toJson();
+		}
+
+		return json;
+	}
+
+	public static ArrayList<Empleado> buscarEmpleados(MongoCollection<Document> collection) throws Exception {
+		ArrayList<Empleado> lista = new ArrayList<>();
+
+		FindIterable<Document> resultados = collection.find();
+
+		for (Document doc : resultados) {
+			String nombre = doc.getString("nombre");
+			Integer edad = doc.getInteger("edad", 0);
+			String puesto = doc.getString("puesto");
+			Double salario = doc.getDouble("salario");
+			Boolean activo = doc.getBoolean("activo", false);
+
+			Empleado empleado = new Empleado(nombre, edad, puesto, salario, activo);
+			lista.add(empleado);
+		}
+
+		return lista;
+	}
+
+}
---
+++ Mongo/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Mongo {
+	requires org.mongodb.driver.sync.client;
+	requires org.mongodb.bson;
+	requires org.mongodb.driver.core;
+}
