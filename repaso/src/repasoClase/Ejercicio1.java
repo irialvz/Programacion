78dbcbdbc5fe5c408a23bee893d01be81cac8461
daw_manana_207 <iriaalvarezsolis@gmail.com>
2025-05-20 12:32:43 +0200

    repaso

---
+++ repaso/src/repasoClase/Ejercicio1.java
@@ -0,0 +1,39 @@
+package repasoClase;
+
+
+import entrada.Teclado;
+
+public class Ejercicio1 {
+	/*public static void main(String[] args) {
+		final int LIMITE = 5;
+		boolean esMenor = true;
+		int[] numerosEntrada = new int[LIMITE];
+		int sumaMultiplos = 0;
+		
+		for (int i=0;i<LIMITE;i++) {
+			numerosEntrada[i] = Teclado.leerEntero("Escribe numero: ");
+		}
+		for (int j = 0; j < numerosEntrada.length && esMenor ;j++) {
+			if (numerosEntrada[j] >= numerosEntrada[j+1] ) {
+				esMenor = false;
+				
+			}
+			if (numerosEntrada[j] % 5 == 0) {
+				sumaMultiplos += numerosEntrada[j];
+			}
+		}
+		if (esMenor) {
+			System.out.println("La secuencia esta ordenada de menor a mayor");
+			System.out.println("La suma de los numeros multiplos de 5 es de: " + sumaMultiplos);
+		} else {
+			System.out.println("La secuencia no esta ordenada de menor a mayor");
+		}
+		
+	}*/
+	public static void main(String[] args) {
+		
+			
+		}
+	}
+
+}
